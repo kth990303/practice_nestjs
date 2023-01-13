5741cc8112fb3188c77d4ca7080be842b6439418
kth990303 <kth990303@naver.com>
2023-01-13 15:55:21 +0900

    feat: create board 기능 구현

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { randomUUID } from 'crypto';
+import { Board, BoardStatus } from './board.model';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +8,17 @@ export class BoardsService {
 
     getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(title: string, description: string): Board {
+        const board: Board = {
+            id: randomUUID(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
     }
 }

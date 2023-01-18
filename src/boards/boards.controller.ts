8ac63486c40671ab6c942ff15e7a7c881d74a252
kth990303 <kth990303@naver.com>
2023-01-18 13:28:20 +0900

    feat: pipe를 이용한 validation 검증 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -13,23 +13,24 @@ export class BoardsController {
     }
 
     @Post('/')
+    @UsePipes(ValidationPipe)
     createBoard(@Body() createBoardDto: CreateBoardDto): Board {
         return this.boardsService.createBoard(createBoardDto);
     }
 
     @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
+    getBoardById(@Param('id', new ParseUUIDPipe({errorHttpStatusCode: HttpStatus.BAD_REQUEST})) id: string): Board {
         return this.boardsService.getBoardById(id);
     }
 
     @Patch('/:id/status')
-    updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus): Board {
+    updateBoardStatus(@Param('id', ParseUUIDPipe) id: string, @Body('status') status: BoardStatus): Board {
         return this.boardsService.updateBoardStatus(id, status);
     }
 
     @Delete('/:id')
     @HttpCode(204)
-    deleteBoard(@Param('id') id: string): void {
+    deleteBoard(@Param('id', ParseUUIDPipe) id: string): void {
         this.boardsService.deleteBoard(id);
     }
 }
